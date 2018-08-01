44f61185582e4dac909b09dc4c4398713d0fac32
Sertac Olgunsoylu <sertac@zeplin.io>
2018-08-01 15:50:29 +0300

    Add publish script

---
+++ packages/extension-style-kit/scripts/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+BASEDIR=$(cd "$(dirname $(dirname "$BASH_SOURCE"))" && pwd)
+BUILDDIR=$BASEDIR/lib
+
+pushd $BASEDIR
+
+npm run build
+
+pushd $BUILDDIR
+
+cp $BASEDIR/package.json ./
+PKG_PATH=$BUILDDIR/$(npm pack)
+rm -f ./package.json
+
+popd
+
+npm publish $PKG_PATH -- $(pwd)
+
+popd
