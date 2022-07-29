1afc8b85c4fd4303f2cd18968d189b30307c6c62
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2022-07-29 15:26:38 -0400

    fix: Fix text style matching algorithm

--- packages/base-extension/codeGenerators/layer.js
+++ packages/base-extension/codeGenerators/layer.js
@@ -7,19 +7,29 @@ import {
     selectorize,
     isHtmlTag,
     getParams,
-    generateIdentifier,
-    getUniqueFirstItems, getResourceContainer
+    getResourceContainer
 } from "zeplin-extension-style-kit/utils";
 
-function findBestConformTextStyle(textStyles, searched) {
+function findBestConformTextStyle(searchedTextStyle, context, params) {
+    const { container } = getResourceContainer(context);
+    if (container.findBestConformingTextStyle) {
+        return container.findBestConformingTextStyle(searchedTextStyle, params.useLinkedStyleguides);
+    }
+
+    // Fallback for backwards compatibility
+    const textStyles = getResources({
+        context,
+        useLinkedStyleguides: params.useLinkedStyleguides,
+        key: "textStyles"
+    });
     let result = { textStyle: null, score: 0 };
     for (const textStyle of textStyles) {
-        if (textStyle.sourceId && searched.sourceId && textStyle.sourceId === searched.sourceId) {
+        if (textStyle.sourceId && searchedTextStyle.sourceId && textStyle.sourceId === searchedTextStyle.sourceId) {
             return textStyle;
         }
 
         const newScore = Object.values(textStyle).filter(value => value !== void 0).length;
-        if (searched.conforms(textStyle) && newScore > result.score) {
+        if (searchedTextStyle.conforms(textStyle) && newScore > result.score) {
             result = {
                 score: newScore,
                 textStyle
@@ -27,6 +37,21 @@ function findBestConformTextStyle(textStyles, searched) {
         }
     }
     return result.textStyle;
+}
+
+function getTextStyleDeclarations(textStyle, layer, context, params) {
+    const matchedTextStyle = findBestConformTextStyle(textStyle, context, params);
+    const declarations = layer.getLayerTextStyleDeclarations(textStyle);
+
+    const textStyleName = matchedTextStyle && matchedTextStyle.name;
+
+    if (params.useMixin && textStyleName && !isHtmlTag(selectorize(textStyleName))) {
+        declarations.push(new Mixin(selectorize(textStyleName).replace(/^\./, "")));
+        const mixinRuleSet = new RuleSet("mixin", layer.getLayerTextStyleDeclarations(matchedTextStyle));
+        return declarations.filter(declaration => !mixinRuleSet.hasProperty(declaration.name));
+    }
+
+    return declarations;
 }
 
 export const layerCodeGenerator = ({
@@ -37,7 +62,6 @@ export const layerCodeGenerator = ({
     } = {}
 }) => (context, selectedLayer) => {
     const params = getParams(context);
-    const { useMixin } = params;
     const { container } = getResourceContainer(context);
     const generator = new Generator(container, params);
 
@@ -47,42 +71,20 @@ export const layerCodeGenerator = ({
     const { defaultTextStyle } = selectedLayer;
 
     if (selectedLayer.type === "text" && defaultTextStyle) {
-        const textStyles = getUniqueFirstItems(getResources({
-            context,
-            useLinkedStyleguides: params.useLinkedStyleguides,
-            key: "textStyles"
-        }), (textStyle, other) => generateIdentifier(textStyle.name) === generateIdentifier(other.name));
-        const containerTextStyle = findBestConformTextStyle(textStyles, defaultTextStyle);
-
-        const declarations = l.getLayerTextStyleDeclarations(defaultTextStyle);
-        const textStyleName = containerTextStyle && containerTextStyle.name;
-
-        if (useMixin && textStyleName && !isHtmlTag(selectorize(textStyleName))) {
-            const mixinRuleSet = new RuleSet("mixin", l.getLayerTextStyleDeclarations(containerTextStyle));
-
-            declarations.forEach(d => {
-                if (!mixinRuleSet.hasProperty(d.name)) {
-                    layerRuleSet.addDeclaration(d);
-                }
-            });
-
-            layerRuleSet.addDeclaration(new Mixin(selectorize(textStyleName).replace(/^\./, "")));
-        } else {
-            declarations.forEach(d => layerRuleSet.addDeclaration(d));
-        }
-
+        const declarations = getTextStyleDeclarations(defaultTextStyle, l, context, params);
+        declarations.forEach(d => layerRuleSet.addDeclaration(d));
         let nameCount = 0;
 
         getUniqueLayerTextStyles(selectedLayer).filter(
             textStyle => !defaultTextStyle.equals(textStyle)
         ).forEach(textStyle => {
-            const font = container.findTextStyleEqual(textStyle, params.useLinkedStyleguides);
+            const font = findBestConformTextStyle(textStyle, context, params);
             const name = font ? font.name : `text-style-${++nameCount}`;
 
             childrenRuleSet.push(
                 new RuleSet(
                     `${selectorize(selectedLayer.name)} ${selectorize(name)}`,
-                    l.getLayerTextStyleDeclarations(textStyle)
+                    getTextStyleDeclarations(textStyle, l, context, params)
                 )
             );
         });
--- packages/zeplin-extension-style-kit/declarations/mixin.js
+++ packages/zeplin-extension-style-kit/declarations/mixin.js
@@ -6,6 +6,10 @@ class Mixin {
     get identifier() {
         return this.id;
     }
+
+    equals(other) {
+        return this.id === other.id;
+    }
 }
 
 export default Mixin;
